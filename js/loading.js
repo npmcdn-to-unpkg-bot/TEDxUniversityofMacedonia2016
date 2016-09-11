951263c5623ce9d8d9136405a486511fb10ead06
kosmastsk <kosmastsk@gmail.com>
2016-09-11 21:34:59 +0300

    marge into master.

---
+++ js/loading.js
@@ -0,0 +1,11 @@
+
+//run the svg animation for 5000 miliseconds
+// Wait for window load
+ $(window).load(function() {
+    // Animate loader off screen
+    $(".loading").fadeOut(2000);
+    $('html, body').css({
+    'overflow': 'auto',
+    'height': 'auto'
+});
+  });
